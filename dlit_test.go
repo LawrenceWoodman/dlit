45085cab9da670077d9bccf1d481fe3320d7566b
Lawrence Woodman <lwoodman@vlifesystems.com>
2016-06-20 06:48:06 +0100

    Replace incorrect use of %T for %t

--- dlit_test.go
+++ dlit_test.go
@@ -150,7 +150,7 @@ func TestInt(t *testing.T) {
 	for _, c := range cases {
 		got, gotIsInt := c.in.Int()
 		if got != c.want || gotIsInt != c.wantIsInt {
-			t.Errorf("Int() with Literal: %s - return: %d, %T - want: %d, %T",
+			t.Errorf("Int() with Literal: %s - return: %d, %t - want: %d, %t",
 				c.in, got, gotIsInt, c.want, c.wantIsInt)
 		}
 	}
@@ -181,7 +181,7 @@ func TestFloat(t *testing.T) {
 	for _, c := range cases {
 		got, gotIsFloat := c.in.Float()
 		if got != c.want || gotIsFloat != c.wantIsFloat {
-			t.Errorf("Float() with Literal: %s - return: %f, %T - want: %f, %T",
+			t.Errorf("Float() with Literal: %s - return: %f, %t - want: %f, %t",
 				c.in, got, gotIsFloat, c.want, c.wantIsFloat)
 		}
 	}
@@ -221,7 +221,7 @@ func TestBool(t *testing.T) {
 	for _, c := range cases {
 		got, gotIsBool := c.in.Bool()
 		if got != c.want || gotIsBool != c.wantIsBool {
-			t.Errorf("Bool() with Literal: %s - return: %T, %T - want: %T, %T",
+			t.Errorf("Bool() with Literal: %s - return: %t, %t - want: %t, %t",
 				c.in, got, gotIsBool, c.want, c.wantIsBool)
 		}
 	}
